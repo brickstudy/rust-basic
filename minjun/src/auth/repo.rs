1bdc64600c41e7784b3d4c8a4e4d5e23a25491ef
robert-min <robertmin522@gmail.com>
2024-11-05 22:33:09 +0900

    feat: ADD delete user with email and ADD test code.

--- minjun/src/auth/repo.rs
+++ minjun/src/auth/repo.rs
@@ -4,6 +4,9 @@ use mysql::params;
 
 use super::signup::SignUpForm;
 
+const DB_CONNECTION_ERROR: &str = "Failed to connect database";
+
+
 #[derive(Debug)]
 pub struct Users {
     pub email: String,
@@ -23,7 +26,7 @@ impl Users {
 
 
 pub fn insert_user(sign_up_form: &SignUpForm) {
-    let mut conn = get_mysql_conn().expect("Failed to connect databases");
+    let mut conn = get_mysql_conn().expect(DB_CONNECTION_ERROR);
     conn.exec_drop(
         "INSERT INTO minjun_users (email, password, username, created_time) 
                 VALUES (:email, :password, :username, :created_time)",
@@ -37,7 +40,8 @@ pub fn insert_user(sign_up_form: &SignUpForm) {
 }
 
 pub fn select_user_with_email(email: &String) -> Option<Users>{
-    let mut conn = get_mysql_conn().expect("Failed to connect databases");
+    let mut conn = get_mysql_conn().expect(DB_CONNECTION_ERROR);
+
     let result = conn.exec_first(
         "SELECT email, password, username from minjun_users
                 WHERE email = :email",
@@ -51,4 +55,55 @@ pub fn select_user_with_email(email: &String) -> Option<Users>{
         password,
         username,
     })
+}
+
+pub fn delete_user_with_email(email: &String) -> Option<String>{ 
+    let mut conn = get_mysql_conn().expect(DB_CONNECTION_ERROR);
+    // 사용자 있는지 확인
+    let result = conn.exec_first(
+        "SELECT email FROM minjun_users WHERE email = :email",
+        params! {
+            "email" => &email
+        }
+    ).expect("Failed to query user from the database.");
+
+    if let Some(existing_email) = result {
+        conn.exec_drop(
+            "DELETE FROM minjun_users WHERE email = :email",
+            params! {
+                "email" => &existing_email,
+            }
+        ).expect("Failed to delete user from the database");
+        
+        return Some(existing_email);
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_crud_user() {
+        // mock
+        let email = "test@example.com";
+
+        let sign_up_form: SignUpForm = SignUpForm::new(
+            email.to_string(),
+            "123455".to_string(),
+            "test_user".to_string()
+        );
+
+        insert_user(&sign_up_form);
+
+        let result = select_user_with_email(&email.to_string());
+        let user = result.unwrap();
+        assert_eq!(user.email, sign_up_form.email);
+        assert_eq!(user.password, sign_up_form.password);
+        assert_eq!(user.username.as_deref(), Some(sign_up_form.username.as_str()));
+
+        // delete 
+        let delete_email = delete_user_with_email(&email.to_string());
+        assert_eq!(delete_email, Some(email.to_string()));
+    }
 }
