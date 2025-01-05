2ab739c4be5a88b64f307f2e54a583f753999393
leejaeseung <wasd222@naver.com>
2025-01-05 09:42:31 +0900

    블록체인 1주차

--- jaeseung-blockchain/src/block/block.rs
+++ jaeseung-blockchain/src/block/block.rs
@@ -1,33 +1,38 @@
-use std::{str::from_utf8, time::{SystemTime, UNIX_EPOCH}};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use sha256::digest;
 
 use rs_merkle::{algorithms::Sha256, Hasher, MerkleTree};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Block {
     header: BlockHeader,
-    data: Vec<String>
+    data: Vec<String>,
 }
 
 impl Block {
-    
-    pub fn new_genesis(height: i32, nonce: i32, data: &Vec<String>) -> Result<Block, String> {
+    const GENESIS_DATA: &str = "This is GENESIS block";
+
+    pub fn new_genesis() -> Result<Block, String> {
+        let data = vec![Self::GENESIS_DATA.to_string()];
+
         Ok(Block {
-            header: BlockHeader::make(height, &["0"; 64].join(""), nonce, &data)?,
-            data: data.clone()
+            header: BlockHeader::make(0, &["0"; 64].join(""), 0, &data)?,
+            data: data,
         })
     }
 
-    pub fn new(height: i32, previous_hash: &String, nonce: i32, data: &Vec<String>) -> Result<Block, String> {
+    pub fn new(previous_block: &Block, nonce: i32, data: &Vec<String>) -> Result<Block, String> {
+        let p_header = &previous_block.header;
+
         Ok(Block {
-            header: BlockHeader::make(height, previous_hash, nonce, data)?,
-            data: data.clone()
+            header: BlockHeader::make(p_header.height + 1, &p_header.hash, nonce, data)?,
+            data: data.clone(),
         })
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct BlockHeader {
     version: String,
     height: i32, // 블록의 높이. 블록 체인에 연결된 블록의 수
@@ -43,9 +48,17 @@ impl BlockHeader {
     const VERSION: &str = "1.0.0";
     const DIFFICULTY: i32 = 0;
 
-    fn make(height: i32, previous_hash: &String, nonce: i32, data: &Vec<String>) -> Result<BlockHeader, String> {
+    fn make(
+        height: i32,
+        previous_hash: &String,
+        nonce: i32,
+        data: &Vec<String>,
+    ) -> Result<BlockHeader, String> {
         let version = Self::VERSION.to_string();
-        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         let difficulty = Self::DIFFICULTY;
         let merkle_root = Self::make_merkle_root(data).ok_or("Merkle Tree Parsing Failed")?;
 
@@ -57,7 +70,7 @@ impl BlockHeader {
             previous_hash: Some(previous_hash.clone()),
             merkle_root,
             nonce,
-            difficulty
+            difficulty,
         })
     }
 
@@ -70,8 +83,36 @@ impl BlockHeader {
         merkle_tree.root_hex()
     }
 
-    fn make_hash(timestamp: u64, merkle_root: &String, previous_hash: &String, nonce: i32) -> String {
-        let target = format!("{}{}{}{}", timestamp.to_string(), merkle_root, previous_hash, nonce.to_string());
+    fn make_hash(
+        timestamp: u64,
+        merkle_root: &String,
+        previous_hash: &String,
+        nonce: i32,
+    ) -> String {
+        let target = format!(
+            "{}{}{}{}",
+            timestamp.to_string(),
+            merkle_root,
+            previous_hash,
+            nonce.to_string()
+        );
         digest(target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_genesis() {
+        let actual = Block::new_genesis().unwrap();
+
+        let data = vec![Block::GENESIS_DATA.to_string()];
+        let expected = Block {
+            header: BlockHeader::make(0, &["0"; 64].join(""), 0, &data).unwrap(),
+            data,
+        };
+        assert_eq!(actual, expected);
     }
 }
--- jaeseung-blockchain/src/main.rs
+++ jaeseung-blockchain/src/main.rs
@@ -1,10 +1,9 @@
 mod block;
 
 use block::Block;
-use rs_merkle::{algorithms::Sha256, MerkleTree};
 
 fn main() {
-    let block = Block::new_genesis(0, 0, &vec![String::from("asdasd")]);
+    let block = Block::new_genesis();
 
     print!("{:?}", block);
 }
